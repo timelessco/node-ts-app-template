61bc78da1104a2a26a4ae6b4d2bfe0a702ea45fe
Navin Moorthy <navin007.a@gmail.com>
2023-05-04 13:49:49 +0530

    chore(config): 🧹 add ignoreDependencies option  in knip and update tsconfig

--- knip.ts
+++ knip.ts
@@ -3,6 +3,7 @@ import { type KnipConfig } from "knip";
 const config: KnipConfig = {
 	project: ["src/**/*.ts!"],
 	entry: ["src/**/*.ts!", "tailwind.config.cjs", "env/**/*", "release-it/**/*"],
+	ignoreDependencies: ["ts-node"],
 };
 
 export default config;
