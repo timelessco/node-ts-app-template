69ac9a5c49c693265df0d97264a4a8b5df4f6725
Navin Moorthy <navin007.a@gmail.com>
2023-07-12 12:54:06 +0530

    docs(ensureDirectoryAccess): 📝 update JSDoc for ensureDirectoryAccess
    - Provide more accurate and detailed information about its purpose and behavior.

--- src/utils/ensureDirectoryAccess.ts
+++ src/utils/ensureDirectoryAccess.ts
@@ -7,17 +7,18 @@ import { throwError } from "./throwError.js";
  * Checks if a directory exists and is readable and writable. If it doesn't exist, it creates it. If it exists but is not readable or writable, it makes it both.
  * @async
  * @function
- * @param {string} directoryPath - The path of the directory to check.
- * @returns {Promise<void>} A promise that resolves to a void indicating whether the directory exists and is readable and writable.
+ * @param {string} directoryPath - The directory path to check.
+ * @returns {Promise<void>}  A promise that resolves to a void indicating whether the directory exists and is readable and writable.
+ * @throws {Error} If the directory does not exist or is not accessible.
  */
 export async function ensureDirectoryAccess(
-	directoryPath: string,
+	directoryPath: string
 ): Promise<void> {
 	try {
 		await fs.access(
 			directoryPath,
 			// eslint-disable-next-line no-bitwise
-			fsSync.constants.F_OK | fsSync.constants.W_OK | fsSync.constants.R_OK,
+			fsSync.constants.F_OK | fsSync.constants.W_OK | fsSync.constants.R_OK
 		);
 	} catch {
 		try {
@@ -26,7 +27,7 @@ export async function ensureDirectoryAccess(
 		} catch (error) {
 			throwError(
 				error,
-				`${directoryPath} does not exists / writable / readable in ensureDirectoryAccess function`,
+				`${directoryPath} does not exists / writable / readable in ensureDirectoryAccess function`
 			);
 		}
 	}
